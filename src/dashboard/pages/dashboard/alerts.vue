dd20b5a548c787c05ddadf7f558ba0f862d65266
weskhaled <weskhaled@gmail.com>
2022-04-29 01:01:53 +0100

    feat: change namespace

---
+++ src/dashboard/pages/dashboard/alerts.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="">
+    <h3>
+      list alerts
+    </h3>
+  </div>
+</template>
+<style lang="less">
+</style>
+<route lang="yaml">
+meta:
+  layout: admin
+</route>
